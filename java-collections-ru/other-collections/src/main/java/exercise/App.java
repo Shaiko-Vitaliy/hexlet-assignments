f550ce09b500ff4c4a3758dbed12efe0f19c7aff
@hexlet/cli <support@hexlet.io>
2022-10-12 11:57:30 +0300

    submit java-collections-ru/other-collections

---
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -0,0 +1,28 @@
+package exercise;
+
+import java.util.*;
+
+// BEGIN
+class App {
+    public static Map<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+        Map<String, String> resultMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Object> key : data1.entrySet()) {
+            if (data2.containsKey(key.getKey())) {
+                if (data2.containsValue(key.getValue())) {
+                    resultMap.put(key.getKey(), "unchanged");
+                } else {
+                    resultMap.put(key.getKey(), "changed");
+                }
+            } else {
+                resultMap.put(key.getKey(), "deleted");
+            }
+        }
+        for (Map.Entry<String, Object> key2 : data2.entrySet()) {
+            if (!data1.containsKey(key2.getKey())) {
+                resultMap.put(key2.getKey(), "added");
+            }
+        }
+        return resultMap;
+    }
+}
+//END
